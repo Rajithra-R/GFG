832f1611f595e7cd7d3bbcc71c1c6f1f613b1e73
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-06-29 14:28:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock buy and sell II/stock-buy-and-sell-ii.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] prices = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.stockBuyAndSell(n, prices);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int stockBuyAndSell(int n, int[] prices) {
+        // code here
+        int buy_price = prices[0];
+        int b_profit = 0;
+        int profit=0;
+        for(int i=0;i<n;i++)
+        {
+        if(prices[i] <= buy_price)   //updating the buy price
+        {
+            buy_price = prices[i];
+        }
+        else   // calculating the profit
+        {
+            profit = prices[i] - buy_price;
+            b_profit += profit;
+            buy_price = prices[i]; // buy on the same day
+        }
+        }
+        return b_profit;
+    }
+}
+        
