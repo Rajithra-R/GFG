025ab34ca0e50ea11d16b9e80dca2b5a3fed67da
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-11 23:43:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Convert to Roman No/convert-to-roman-no.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int N = sc.nextInt();
+			
+			Solution ob = new Solution();
+			System.out.println(ob.convertToRoman(N));
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+	String convertToRoman(int n) {
+		//code here
+		String units[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String hun[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String thou[] = {"" ,"M","MM","MMM"};
+        return (thou[n/1000] + hun[(n/100)%10] + tens[(n/10)%10] + units[n%10]);
+	}
+}
